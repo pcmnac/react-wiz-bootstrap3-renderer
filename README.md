d4c2b5f020e08a09d1fb2a5609d4de5ae93c2c20
Paulo Cesar Moura Nunes Americo Coutinho <paulo.coutinho@serpro.gov.br>
2017-10-17 15:22:43 -0300

    initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# react-wizard
+
+Style-free wizard component for React apps.
+
+
+## License
+
+*react-wizard* is available under MIT. See LICENSE for more details.
+
---
+++ __tests__/boilerplate_test.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import {
+//   renderIntoDocument,
+//   findRenderedDOMComponentWithClass,
+//   findRenderedDOMComponentWithTag,
+//   Simulate
+// } from 'react-addons-test-utils';
+import { expect } from 'chai';
+
+import * as bs3 from '../src/bs3Renderer.js';
+
+describe('Boilerplate', function () {
+  it('should do boilerplate things', function () {
+    // TODO: test something now
+    expect(true).to.equal(true);
+  });
+});
